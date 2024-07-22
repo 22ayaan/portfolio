a5e4d2a29493d1749af008c694cf654d41b8f212
Ayaan Siddiqui <ars394@pitt.edu>
2024-07-22 12:52:59 +0530

    remove unnecessary folders and fix overflow hero section issue.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,10 +16,7 @@ const Hero = () => {
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
           fill="white"
         />
-        <Spotlight
-          className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="purple"
-        />
+        <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple" />
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
@@ -57,7 +54,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Ayaan, a Next.js Developer based in Croatia.
           </p>
 
           <a href="#about">
